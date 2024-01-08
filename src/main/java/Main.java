8aed489047153b1831f0dcb13e341e109ae02c8f
Robin <152497862+Ramino62779@users.noreply.github.com>
2024-01-08 11:46:03 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import GUI.Connect4;
 
 public class Main {
     public static void main(String[] args) {
+        //VierGewinnt vg = new VierGewinnt(5, Difficulty.medium);
+        //vg.play();
         Connect4.getInstance();
     }
 }
