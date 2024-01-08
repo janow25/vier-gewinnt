2e2b14585abe98fdb4e086388f1b5863b508e9ba
robin.remte <robin.remte@11880.com>
2024-01-08 11:39:54 +0100

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 import GUI.Connect4;
 
-import java.util.Scanner;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Connect4.getInstance();
