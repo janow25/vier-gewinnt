80f8f9dd453cb110acee1341e92d5a334743c326
Janne Nowak <janne.now@icloud.com>
2024-01-23 16:24:43 +0100

    fixed formatting

--- src/test/java/GUI/Connect4GUITest.java
+++ src/test/java/GUI/Connect4GUITest.java
@@ -15,44 +15,44 @@ public class Connect4GUITest {
     private static final int DEFAULT_ROWS = 6;
     private static final int DEFAULT_COLUMNS = 7;
 
-        @Test
-        public void getInstance() {
-            Connect4GUI instance1 = Connect4GUI.getInstance();
-            Connect4GUI instance2 = Connect4GUI.getInstance();
+    @Test
+    public void getInstance() {
+        Connect4GUI instance1 = Connect4GUI.getInstance();
+        Connect4GUI instance2 = Connect4GUI.getInstance();
 
-            // Check if both instances are not null and the same
-            assertNotNull(instance1);
-            assertNotNull(instance2);
-            assertSame(instance1, instance2);
-        }
-
-        @Test
-        public void createGUI() {
-            Connect4GUI instance = Connect4GUI.getInstance();
-            instance.createGUI();
-            assertNotNull(instance.getFrame());
-            assertNotNull(instance.getPanel());
-        }
-
-        @Test
-        void createNewBoard() {
-        }
-
-        @Test
-        void hasPlayerWon() {
-            Connect4GUI instance = Connect4GUI.getInstance();
-
-            assertFalse(instance.hasPlayerWon());
-            instance.setPlayerWon(true);
-            assertTrue(instance.hasPlayerWon());
-            instance.setPlayerWon(false);
-            assertFalse(instance.hasPlayerWon());
-        }
-
-        @Test
-        void setPlayerWon() {
-        }
+        // Check if both instances are not null and the same
+        assertNotNull(instance1);
+        assertNotNull(instance2);
+        assertSame(instance1, instance2);
     }
+
+    @Test
+    public void createGUI() {
+        Connect4GUI instance = Connect4GUI.getInstance();
+        instance.createGUI();
+        assertNotNull(instance.getFrame());
+        assertNotNull(instance.getPanel());
+    }
+
+    @Test
+    void createNewBoard() {
+    }
+
+    @Test
+    void hasPlayerWon() {
+        Connect4GUI instance = Connect4GUI.getInstance();
+
+        assertFalse(instance.hasPlayerWon());
+        instance.setPlayerWon(true);
+        assertTrue(instance.hasPlayerWon());
+        instance.setPlayerWon(false);
+        assertFalse(instance.hasPlayerWon());
+    }
+
+    @Test
+    void setPlayerWon() {
+    }
+}
 
 
 
